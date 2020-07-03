4cbeb33c804e466e476b8315f96ce89546ea7063
Alexandr Bubeshko <a.bubeshko@gmail.com>
2020-07-03 13:16:34 +0300

    group team routes

--- routes/api.php
+++ routes/api.php
@@ -13,21 +13,27 @@ $router->group([
     $router->post('logout', 'AuthController@logout');
     /** @uses \App\Http\Controllers\Api\AuthController::getCurrentUserData() */
     $router->post('me', 'AuthController@getCurrentUserData');
-    /** @uses \App\Http\Controllers\Api\ReviewController::getFeedForEmployee() */
-    $router->get('feed', 'ReviewController@getFeedForEmployee');
-    $router->get('feed/size/{size}', 'ReviewController@getFeedForEmployee');
-    /** @uses \App\Http\Controllers\Api\ReviewController::show() */
-    $router->get('feed/{feedId}', 'ReviewController@show');
-    /** @uses \App\Http\Controllers\Api\UserController::getUsersWithPagination() */
-    $router->get('users', 'UserController@getUsersWithPagination');
-    /** @uses \App\Http\Controllers\Api\UserController::getUserFeed() */
-    $router->get('users/{userId}/feed', 'UserController@getUserFeed');
-    /** @uses \App\Http\Controllers\Api\UserController::getListOfUsers() */
-    $router->get('users/list', 'UserController@getListOfUsers');
-    /** @uses \App\Http\Controllers\Api\UserController::getDataForFilter() */
-    $router->get('users/filter-data', 'UserController@getDataForFilter');
-    /** @uses \App\Http\Controllers\Api\UserController::getUser() */
-    $router->get('users/{userId}', 'UserController@getUser');
-    /** @uses \App\Http\Controllers\Api\ReviewController::addNewReviewToUser() */
-    $router->post('users/{userId}/reviews', 'ReviewController@addNewReviewToUser');
+
+    $router->group(['prefix' => 'feed'], function () use ($router) {
+        /** @uses \App\Http\Controllers\Api\ReviewController::getFeedForEmployee() */
+        $router->get('/', 'ReviewController@getFeedForEmployee');
+        $router->get('/size/{size}', 'ReviewController@getFeedForEmployee');
+        /** @uses \App\Http\Controllers\Api\ReviewController::show() */
+        $router->get('/{feedId}', 'ReviewController@show');
+    });
+
+    $router->group(['prefix' => 'users'], function () use ($router) {
+        /** @uses \App\Http\Controllers\Api\UserController::getUsersWithPagination() */
+        $router->get('/', 'UserController@getUsersWithPagination');
+        /** @uses \App\Http\Controllers\Api\UserController::getUserFeed() */
+        $router->get('/{userId}/feed', 'UserController@getUserFeed');
+        /** @uses \App\Http\Controllers\Api\UserController::getListOfUsers() */
+        $router->get('/list', 'UserController@getListOfUsers');
+        /** @uses \App\Http\Controllers\Api\UserController::getDataForFilter() */
+        $router->get('/filter-data', 'UserController@getDataForFilter');
+        /** @uses \App\Http\Controllers\Api\UserController::getUser() */
+        $router->get('/{userId}', 'UserController@getUser');
+        /** @uses \App\Http\Controllers\Api\ReviewController::addNewReviewToUser() */
+        $router->post('/{userId}/reviews', 'ReviewController@addNewReviewToUser');
+    });
 });
